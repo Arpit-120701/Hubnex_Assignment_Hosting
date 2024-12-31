5a0b6cfbe34acb81ca05ea78b9efcfcd6338cdaa
Arpit_Sangamnerkar <arpit99.sangamnerkar@gmail.com>
2024-12-31 19:36:31 +0530

    vercel file added

--- Backend/Server.js
+++ Backend/Server.js
@@ -8,7 +8,13 @@ const connectDB  = require('./db/dbConnect')
 
 const app = express()
 
-app.use(cors());
+app.use(cors(
+    {
+        origin : [ "https://deploy-mern-1whq.vercel.app"],
+        methods:["POST", "GET"],
+        credentials:true
+    }
+));
 
 dotenv.config()
 
