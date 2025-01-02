8ef8c1eb2a93f2d6c201613818319dd0ae034d0a
Arpit_Sangamnerkar <arpit99.sangamnerkar@gmail.com>
2025-01-02 22:29:05 +0530

    ==

--- Backend/Server.js
+++ Backend/Server.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin:["https://hubnex-assignment-hosting.vercel.app"],
+        origin:"https://hubnex-assignment-hosting.vercel.app",
         methods:["POST","GET"],
         credentials:true
     }
