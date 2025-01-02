e3a454b81c460baca9b4a7c58d164e3c7c247f45
Arpit_Sangamnerkar <arpit99.sangamnerkar@gmail.com>
2025-01-02 16:14:35 +0530

    mongo uri changed

--- Backend/db/dbConnect.js
+++ Backend/db/dbConnect.js
@@ -4,7 +4,7 @@ const colors = require('colors')
 const connectDB =  async() => {
     try
     {
-        const conn = await mongoose.connect(process.env.MONGO_SECRET_URL)
+        const conn = await mongoose.connect("mongodb+srv://arpit99sangamnerkar:hubnexlabs@cluster0.2jubf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log(`MongoDb Connected !!`.cyan.bold)
     }
     catch(error)
