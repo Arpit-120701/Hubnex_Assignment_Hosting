15f6374835437da372de2963a480f76931c4df5e
Arpit_Sangamnerkar <arpit99.sangamnerkar@gmail.com>
2025-01-09 20:30:46 +0530

    cors added

--- Backend/Server.js
+++ Backend/Server.js
@@ -11,7 +11,14 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors(
+    {
+        origin:["http://localhost:3000"],
+        methods:["POST","GET"],
+        allowedHeaders: 'Content-Type,Authorization',
+        credentials:true
+    }
+))
 
 app.use(express.json())
 
